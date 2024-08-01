afa3778eb635c7d966c4c87d42a35255fee4cc83
pbbaba <pbaba1@gsu.edu>
2024-08-01 10:42:18 -0400

    added script to prepare json and yaml files

---
+++ json_yaml_files/prepare_files.py
@@ -0,0 +1,178 @@
+from bids import BIDSLayout
+from collections import defaultdict
+import re
+import json
+import sys
+import yaml
+from pathlib import Path
+
+
+class BIDStoNDAConfigGenerator:
+    def __init__(self):
+        self.path = sys.argv[1]
+        self.layout = BIDSLayout(self.path, derivatives=True)
+        self.A = 'image03'
+        self.pattern = r'^(?P<A>[^_]+)_(?P<X>[^.]+)\.(?P<Y>[^.]+)\.(?P<Z>[^.]+)\..+$'
+        # self.top_level_files = ['CHANGES',
+        #                         'README', 'dataset_description.json']
+        self.scan_type = {
+            'anat': {
+                'mprage': 'MR structural (MPRAGE)',
+                't1w': 'MR structural (T1)',
+                'pd': 'MR structural (PD)',
+                'fspgr': 'MR structural (FSPGR)',
+                'fsip': 'MR structural (FISP)',
+                't2w': 'MR structural (T2)',
+                'pd_t2': 'MR structural (PD, T2)',
+                'b0_map': 'MR structural (B0 map)',
+                'b1_map': 'MR structural (B1 map)',
+                'flash': 'MR structural (FLASH)',
+                'mp2rage': 'MR structural (MP2RAGE)',
+                'tse': 'MR structural (TSE)',
+                't1w_t2w': 'MR structural (T1, T2)',
+                'mpnrage': 'MR structural (MPnRAGE)'
+            },
+            'pet': {
+                'pet': 'PET'
+            }
+        }
+        self.image_modality = {
+            'pet': 'PET',
+            'anat': 'MRI',
+            'func': 'MRI',
+            'dwi': 'MRI',
+            'fmap': 'MRI'
+        }
+
+    def extract_file_name_components(self, file_name):
+        match = re.match(self.pattern, file_name)
+        if match:
+            A = match.group('A')
+            X = match.group('X')
+            Y = match.group('Y')
+            Z = match.group('Z')
+            return A, X, Y, Z
+        else:
+            return None
+
+    def prepare_json_contents(self, file_name):
+        # json_contents = {file: file for file in self.top_level_files}
+        json_contents = {}
+        _, X, Y, _ = self.extract_file_name_components(file_name)
+        files = self.layout.get(
+            scope=X if X != 'inputs' else 'raw', datatype=Y, return_type='file')
+        for file_path in files:
+            sub_index = file_path.find('derivatives')
+            if sub_index == -1:
+                sub_index = file_path.find('sub-')
+            if sub_index != -1:
+                sub_path = file_path[sub_index:]
+                entities = self.layout.parse_file_entities(sub_path)
+                subject = entities.get('subject')
+                session = entities.get('session')
+                if subject:
+                    sub_path = sub_path.replace(subject, '{SUBJECT}')
+                if session:
+                    sub_path = sub_path.replace(session, '{SESSION}')
+                sub_path = re.sub(r'-\d', '{#}', sub_path)
+                json_contents[sub_path] = sub_path
+        finalPath = '/'.join(self.path.split('/')[:-2]) + '/prepared_jsons'
+        print(finalPath)
+        Path(finalPath).mkdir(parents=True, exist_ok=True)
+        with open(f'{finalPath}/'+file_name, 'w') as f:
+            json.dump(json_contents, f)
+
+    def fetch_scan_type(self, Y, Z):
+        scan_type = ''
+        if Z.lower() in self.scan_type[Y]:
+            scan_type = self.scan_type[Y][Z.lower()]
+        elif '_' in Z:
+            first = Z.split('_')[0]
+            if first.lower() in self.scan_type[Y]:
+                scan_type = self.scan_type[Y][first.lower()]
+        if Y in self.scan_type[Y]:
+            scan_type = self.scan_type[Y][Y]
+        return scan_type
+
+    def prepare_yaml_contents(self, file_name):
+        _, _, Y, Z = self.extract_file_name_components(file_name)
+
+        yaml_contents = {
+            "scan_type": self.fetch_scan_type(Y, Z),
+            "scan_object": 'LIVE',
+            "image_file_format": 'NIFTI',
+            "image_modality": self.image_modality[Y],
+            "transformation_performed": 'Yes'
+        }
+        finalPath = '/'.join(self.path.split('/')[:-2]) + '/prepared_yamls'
+        Path(finalPath).mkdir(parents=True, exist_ok=True)
+        with open(f'{finalPath}/'+file_name, 'w') as f:
+            yaml.dump(yaml_contents, f)
+
+    def fetch_Z_value(self, files):
+        Z = set()
+        ignore_list = ['desc', 'subject', 'session',
+                       'extension', 'suffix', 'datatype']
+        for file in files:
+            entities = self.layout.parse_file_entities(file)
+            # print(entities)
+            filtered_entities = {k: v for k,
+                                 v in entities.items() if k not in ignore_list}
+            # print(filtered_entities)
+            for key, value in filtered_entities.items():
+                if key != 'space':
+                    z = key + '-' + value
+                else:
+                    z = value
+                if ({'suffix', 'datatype'} <= entities.keys() and entities['suffix'] != entities['datatype']) or \
+                        ('suffix' in entities and 'datatype' not in entities):
+                    if z:
+                        z = z + '_'
+                    z = z + entities['suffix']
+                if z:
+                    Z.add(z)
+            if not Z and entities['suffix'] != entities['datatype']:
+                Z.add(entities['suffix'])
+        return list(Z)
+
+    def prepare_file_names(self, X, Y, Z):
+        file_names = defaultdict(set)
+        filename = f"{self.A}_{X}"
+        file_base = f"{filename}.{Y}."
+        for z in Z:
+            file_names['json'].add(file_base + z + '.json')
+            file_names['yaml'].add(file_base + z + '.yaml')
+        file_names['json'] = list(file_names['json'])
+        file_names['yaml'] = list(file_names['yaml'])
+        return file_names
+
+    def run(self):
+        X = []
+        Xi = self.layout.get(scope='raw')  # inputs
+        # Xd = self.layout.get(scope='derivatives')  # derivatives
+        Xs = self.layout.get(scope='sourcedata')  # sourcedata
+        if Xi:
+            X.append('inputs')
+        # if Xd:
+        #     X.append('derivatives')
+        if Xs:
+            X.append('sourcedata')
+
+        Y_types = self.layout.get_datatypes()
+
+        for x in X:
+            for y in Y_types:
+                files = self.layout.get(
+                    scope=x if x != 'inputs' else 'raw', datatype=y, return_type='file')
+                Z = self.fetch_Z_value(files)
+                file_names = self.prepare_file_names(X=x, Y=y, Z=Z)
+                for file in file_names['json']:
+                    self.prepare_json_contents(file)
+                for file in file_names['yaml']:
+                    self.prepare_yaml_contents(file)
+
+
+# Example usage:
+# path = '/Users/pbaba1/Downloads/NDA_Dataset/ds004733/'
+generator = BIDStoNDAConfigGenerator()
+generator.run()
